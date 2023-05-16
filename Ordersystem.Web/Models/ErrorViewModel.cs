e1dd67d3bd89722fb0a6ca65c8e93fa788fab027
Khasan Alibekov <borz2010@hotmail.com>
2023-05-16 16:23:20 +0200

    Add project files.

---
+++ Ordersystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ordersystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
